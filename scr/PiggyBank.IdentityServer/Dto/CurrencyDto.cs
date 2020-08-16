4440fd9cd65328bee3802092201bdb4a7565e586
Denis Makarenko <denis@bridgerpay.com>
2020-08-16 14:53:24 +0300

    Added  available currencies

--- scr/PiggyBank.IdentityServer/Dto/CurrencyDto.cs
+++ scr/PiggyBank.IdentityServer/Dto/CurrencyDto.cs
@@ -1,13 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace PiggyBank.IdentityServer.Dto
+﻿namespace PiggyBank.IdentityServer.Dto
 {
     public class CurrencyDto
     {
-        [Required]
-        public string PreviousCurrency { get; set; }
-
-        [Required]
-        public string NewCurrency { get; set; }
+        
     }
 }
