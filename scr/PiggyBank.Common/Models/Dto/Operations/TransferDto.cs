690fc8a658635d4027a7d503cbecb7a9f583597f
Denis Makarenko <denis-makarenko@outlook.com>
2021-02-21 20:33:54 +0300

    PB-57 Some fix

--- scr/PiggyBank.Common/Models/Dto/Operations/TransferDto.cs
+++ scr/PiggyBank.Common/Models/Dto/Operations/TransferDto.cs
@@ -2,8 +2,6 @@ namespace PiggyBank.Common.Models.Dto.Operations
 {
     public class TransferDto : OperationBase
     {
-        public string Comment { get; set; }
-
         public int FromId { get; set; }
 
         public int ToId { get; set; }
